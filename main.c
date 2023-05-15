5c2ffbdb1923bd9d9eac0ac786f917cfa1ef355c
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-15 21:57:27 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-int main() {
-   
-   printf("Hello, World!");
-   return 0;
-}
 
+int main() {
+    int s;
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &s);
+    
+    int a[s];
+    
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < s; i++) {
+       
+        scanf("%d", &a[i]);
+    }
+    
+    printf("Elements in the array:\n");
+    for (int i = 0; i < s; i++) {
+        if(a[i]>0)
+        printf("%d ", a[i]);
+    }
+    
+    return 0;
+}
