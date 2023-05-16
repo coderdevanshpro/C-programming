7de9aff944e6977020f4eebcf297c4053c4bc4d7
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-16 22:20:43 +0530

    Create sum of lower triangular.c

---
+++ sum of lower triangular.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int size, i, j, sum = 0;
+
+    printf("Enter the size of the matrix: ");
+    scanf("%d", &size);
+
+    int matrix[size][size];
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate the sum of the lower triangular matrix
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < i; j++) {
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("Sum of the lower triangular matrix: %d\n", sum);
+
+    return 0;
+}
