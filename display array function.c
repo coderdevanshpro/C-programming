8723dfb679bb091176fa1d51701d15551d714554
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-18 18:10:38 +0530

    Create display array function.c

---
+++ display array function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void displayArray(int arr[], int size) {
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    displayArray(arr, size);
+
+    return 0;
+}
