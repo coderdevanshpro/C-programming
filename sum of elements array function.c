b0f147e90d484c88651c89d0a904eeee773dce4e
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-18 18:11:10 +0530

    Create sum of elements array function.c

---
+++ sum of elements array function.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sumOfArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = sumOfArray(arr, size);
+    printf("Sum of array elements is %d\n", sum);
+
+    return 0;
+}
