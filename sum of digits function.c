086ddec92671a0c97525c320b1381e0b8db30523
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-18 18:08:03 +0530

    Create sum of digits function.c

---
+++ sum of digits function.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int sum = sumOfDigits(num);
+    printf("The sum of digits of %d is %d\n", num, sum);
+
+    return 0;
+}
