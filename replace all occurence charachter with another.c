99639184fca997c00045c368d740c99fb541b678
coderdevanshpro <114148027+coderdevanshpro@users.noreply.github.com>
2023-05-18 19:53:13 +0530

    Create replace all occurence charachter with another.c

---
+++ replace all occurence charachter with another.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void replaceAllOccurrences(char str[], char oldChar, char newChar) {
+    int i;
+    int length = strlen(str);
+    for (i = 0; i < length; i++) {
+        if (str[i] == oldChar) {
+            str[i] = newChar;
+        }
+    }
+}
+
+int main() {
+    char str[100];
+    char oldChar, newChar;
+    printf("Enter a string: ");
+    gets(str);
+    printf("Enter the character to replace: ");
+    scanf("%c", &oldChar);
+    getchar(); // Read the newline character
+    printf("Enter the new character: ");
+    scanf("%c", &newChar);
+    replaceAllOccurrences(str, oldChar, newChar);
+    printf("String after replacing all occurrences of %c with %c: %s\n", oldChar, newChar, str);
+    return 0;
+}
